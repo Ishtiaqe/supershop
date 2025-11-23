060c4a21474611061e4c994251769251f878c568
Ishtiaqe Hanif <ishtiaqe.hanif@gigalogy.com>
2025-11-23 11:34:44 +0600

    test 3

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,20 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 const config: CapacitorConfig = {
   appId: 'com.supershop.app',
   appName: 'SuperShop',
-  webDir: 'supershop-frontend/out',
-  server: {
-    androidScheme: 'https',
-    url: 'http://10.0.2.2:3000', // Special alias for localhost in Android Emulator
-    cleartext: true
-  }
+  webDir: 'out',
+  server: isDev
+    ? {
+        androidScheme: 'https',
+        url: 'http://10.0.2.2:3000', // Android emulator loopback
+        cleartext: true,
+      }
+    : {
+        androidScheme: 'https',
+      },
 };
 
 export default config;
