d5b68e465a83dbd539c2e10e1cc01aef0446b030
Ishtiaqe Hanif <ishtiaqe.hanif@gigalogy.com>
2025-11-28 21:22:23 +0600

    google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import "./globals.css";
 import "antd/dist/reset.css";
 import { Providers } from "@/components/providers";
 import dynamic from "next/dynamic";
+import Script from "next/script";
+
+const Shell = dynamic(() => import("@/components/shell/Shell"), { ssr: false });
 
 const Shell = dynamic(() => import("@/components/shell/Shell"), { ssr: false });
 
@@ -37,6 +40,22 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Providers>
+          {process.env.NEXT_PUBLIC_GA_TRACKING_ID && (
+            <>
+              <Script
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+                strategy="afterInteractive"
+              />
+              <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}');
+                `}
+              </Script>
+            </>
+          )}
           <Shell>{children}</Shell>
         </Providers>
       </body>
