460a13fa0f3a5ec19611072e6fcb4cba8f7d455d
Ishtiaqe Hanif <ishtiaqe.hanif@gigalogy.com>
2026-03-18 00:20:29 +0600

    force dynamic

--- src/app/api/proxy/analytics/summary/route.ts
+++ src/app/api/proxy/analytics/summary/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(req: NextRequest) {
   try {
     // Get authorization token from header
