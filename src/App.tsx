92bc083bd0127b195ac16da1c36f8e3f2a87d4e4
Ishtiaqe Hanif <ishtiaqe.hanif@gigalogy.com>
2026-06-27 13:52:47 +0600

    trigger

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { RouterProvider } from 'react-router-dom';
-import { Providers } from './components/providers';
-import { router } from './router';
+import { RouterProvider } from "react-router-dom";
+import { Providers } from "./components/providers";
+import { router } from "./router";
 
 export default function App() {
   return (
